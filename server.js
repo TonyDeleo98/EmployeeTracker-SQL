9ffbdf3be3d5a3b5254709d5b4fcd7c761eb756b
TonyDeleo98 <Tonyd9819@gmail.com>
2023-11-07 17:05:06 -0500

    updated files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Phillies_21",
     database: "employeeTracker_db",
 });
 
